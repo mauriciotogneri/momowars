149c2d6af2281d76cc72cf8dad89f574ce804c7c
mtogneri <mtogneri@openwt.com>
2017-10-21 14:37:34 +0200

    Added two columns in game screen

--- public/js/activities/game.js
+++ public/js/activities/game.js
@@ -1,8 +1,12 @@
 app.controller(CONTROLLER_GAME, function($scope, database, databaseGame, ui)
 {
+	$scope.loading = true
+
 	$scope.init = function()
 	{
 		// TODO
+		$scope.loading = false
+		$scope.$applyAsync()
 	}
 
 	$scope.init()
