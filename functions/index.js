63b6728653c3994afb3df08d02819b46b124efe9
mtogneri <mtogneri@openwt.com>
2017-10-25 23:36:52 +0200

    Added base login end point

--- functions/index.js
+++ functions/index.js
@@ -8,9 +8,30 @@ admin.initializeApp({
 })
 
 const functions = require('firebase-functions')
+const express   = require('express')
+const shajs     = require('sha.js')
 const database  = new (require('./database/database.js'))(admin)
+const app = express()
 
 // =============================================================================
+
+app.post('/v1/login', (request, response) =>
+{
+	const sessionId = new shajs.sha512().update(new Date().getTime()).digest('hex')
+
+	return response
+				.status(200)
+				.set('Session-Token', sessionId)
+				.json({foo:"bar"})
+})
+
+const api = express()
+api.use('/api', app)
+
+exports.api = functions.https.onRequest(api)
+
+
+//console.log()
 
 //exports.onTurnFinished = functions.database.ref('games/{gameId}').onUpdate(event =>
 //{
