bbd92c10a9d2ddb1eb2609c816349311ff7628ec
mtogneri <mtogneri@openwt.com>
2017-11-10 23:29:33 +0100

    Added constants

---
+++ src/main/kotlin/com/mauriciotogneri/momowars/constants/CellType.kt
@@ -0,0 +1,14 @@
+package com.mauriciotogneri.momowars.constants
+
+enum class CellType
+{
+    BASE,
+    GRASS,
+    EARTH,
+    MOUNTAIN,
+    FOREST,
+    DESERT,
+    CONCRETE,
+    WATER,
+    ICE
+}
---
+++ src/main/kotlin/com/mauriciotogneri/momowars/constants/UnitType.kt
@@ -0,0 +1,11 @@
+package com.mauriciotogneri.momowars.constants
+
+enum class UnitType
+{
+    SQUAD,
+    MORTAR,
+    HUMVEE,
+    LIGHT_TANK,
+    HEAVY_TANK,
+    HELICOPTER
+}
